5cfc93555e63e0ef1e615a7cb978162e5550e8e0
DenisMPar <paradadenismartin@gmail.com>
2021-10-05 16:56:31 -0300

    beta v1.8

--- index.ts
+++ index.ts
@@ -122,7 +122,7 @@ app.post("/rooms/:roomId/online", (req, res) => {
 
 app.use(express.static("../dist"));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(__dirname + "/dist/index.html");
 });
 
 app.listen(port, () => {
